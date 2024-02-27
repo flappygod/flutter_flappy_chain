4f99b42d9b03b34683ff9e943df95fd6c35f073d
lilin <327603258@qq.com>
2024-02-27 14:36:24 +0800

    add flutter flappy chain usdt token balance

--- lib/flutter_flappy_chain.dart
+++ lib/flutter_flappy_chain.dart
@@ -4,8 +4,24 @@ import 'package:web_socket_channel/io.dart';
 import 'package:bip39/bip39.dart' as bip39;
 import 'package:web3dart/web3dart.dart';
 import 'package:flutter/services.dart';
+import 'package:decimal/decimal.dart';
+import 'package:dio/dio.dart' as dio;
 import 'package:http/http.dart';
 import 'dart:async';
+import 'dart:math';
+
+class TrxBalance {
+  ///this is balance
+  Decimal balance = Decimal.fromInt(0);
+
+  ///this is contract balance
+  Map<String, Decimal> contractBalance = {};
+
+  ///get balance
+  Decimal getContractBalance(String contactAddress, {int wei = 6}) {
+    return ((contractBalance[contactAddress] ?? Decimal.fromInt(0)) / Decimal.fromInt(pow(10, 6).toInt())).toDecimal();
+  }
+}
 
 class FlutterFlappyChain {
   ///channel
@@ -61,17 +77,49 @@ class FlutterFlappyChain {
   }
 
   ///get trx balance
-  static getTrxBalance(String address) {}
+  static Future<TrxBalance> getTrxBalance(String address) async {
+    TrxBalance trxBalance = TrxBalance();
+
+    ///use dio
+    dio.Dio dioRequest = dio.Dio();
+    dioRequest.options.responseType = dio.ResponseType.json;
+    String urlPath = "https://api.trongrid.io/v1/accounts/$address";
+    dio.Response response = await dioRequest.get(urlPath);
+
+    ///return data
+    if (response.data['data'] != null || response.data['data'] is List || response.data['data'].isNotEmpty) {
+      ///set data
+      Decimal decimal = Decimal.fromInt(response.data['data'][0]['balance']);
+      Decimal divisor = Decimal.parse('1000000');
+      trxBalance.balance = (decimal / divisor).toDecimal();
+    }
+
+    ///set trc20 data list
+    if (response.data['data'] != null || response.data['data'] is List || response.data['data'].isNotEmpty) {
+      List trc20List = response.data['data'][0]['trc20'];
+      Map<String, Decimal> balanceList = {};
+      for (Map item in trc20List) {
+        String key = item.keys.first;
+        String value = item[key];
+        balanceList[key] = Decimal.parse(value);
+      }
+      trxBalance.contractBalance = balanceList;
+    }
+    return trxBalance;
+  }
 
   ///get eth Balance
-  static Future<EtherAmount> getEthBalance(String address) async {
+  static Future<Decimal> getEthBalance(String address) async {
     String rpcUrl = 'https://eth-mainnet.token.im';
     String wsUrl = 'ws://eth-mainnet.token.im';
     Client httpClient = Client();
     Web3Client web3client = Web3Client(rpcUrl, httpClient, socketConnector: () {
       return IOWebSocketChannel.connect(wsUrl).cast<String>();
     });
-    return await web3client.getBalance(EthereumAddress.fromHex(address));
+    EtherAmount amount = await web3client.getBalance(EthereumAddress.fromHex(address));
+    Decimal decimal = Decimal.fromBigInt(amount.getInWei);
+    Decimal divisor = Decimal.parse('1000000000000000000');
+    return (decimal / divisor).toDecimal();
   }
 
   ///get eth Balance
@@ -83,7 +131,9 @@ class FlutterFlappyChain {
       return IOWebSocketChannel.connect(wsUrl).cast<String>();
     });
     DeployedContract contract = DeployedContract(
-      ContractAbi.fromJson('[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_upgradedAddress","type":"address"}],"name":"deprecate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"deprecated","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_evilUser","type":"address"}],"name":"addBlackList","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"upgradedAddress","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balances","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"maximumFee","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_maker","type":"address"}],"name":"getBlackListStatus","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowed","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newBasisPoints","type":"uint256"},{"name":"newMaxFee","type":"uint256"}],"name":"setParams","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"issue","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"redeem","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"basisPointsRate","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"isBlackListed","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_clearedUser","type":"address"}],"name":"removeBlackList","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"MAX_UINT","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_blackListedUser","type":"address"}],"name":"destroyBlackFunds","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_initialSupply","type":"uint256"},{"name":"_name","type":"string"},{"name":"_symbol","type":"string"},{"name":"_decimals","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"Issue","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"Redeem","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"newAddress","type":"address"}],"name":"Deprecate","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"feeBasisPoints","type":"uint256"},{"indexed":false,"name":"maxFee","type":"uint256"}],"name":"Params","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_blackListedUser","type":"address"},{"indexed":false,"name":"_balance","type":"uint256"}],"name":"DestroyedBlackFunds","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_user","type":"address"}],"name":"AddedBlackList","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_user","type":"address"}],"name":"RemovedBlackList","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"}]', 'USDT'),
+      ContractAbi.fromJson(
+          '[{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"type":"function"}]',
+          'USDT'),
       EthereumAddress.fromHex(contractAddress),
     );
     ContractFunction balanceFunction = contract.function('balanceOf');
@@ -92,6 +142,8 @@ class FlutterFlappyChain {
       function: balanceFunction,
       params: [EthereumAddress.fromHex(address)],
     );
-    return balance.first;
+    Decimal decimal = Decimal.fromBigInt(balance.first);
+    Decimal divisor = Decimal.parse('1000000');
+    return (decimal / divisor).toDecimal();
   }
 }
